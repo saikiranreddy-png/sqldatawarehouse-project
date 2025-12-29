5e82244511b521b662baf7b71f94e197a3ca8499
saikiranreddy <saikiranreddysingasani7075@gmail.com>
2025-12-29 10:32:30 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,16 @@
+/* script purpose: this script creates a new database  named "datawarehouse"  . this script consists of three schemas like bronze,sliver and gold. */
+use master;
+go
+create database datawarehouse;
+go
+use datawarehouse;
+go
+
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
 
